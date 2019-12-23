6dd6587ac3ce1b58e0b199a229d327551964dd7d
dannymendoza <daniel.mendoza@alumnos.udg.mx>
2019-12-23 15:20:27 -0600

    Print the name of the file and the folder
    It seems to be working well

--- readPdf.sh
+++ readPdf.sh
@@ -3,6 +3,9 @@
 # Daniel Mendoza
 # Robotics Engineer
 
+
+# Notify-send display the folder and file that you select!
+
 IFS=$'\n'
 declare -a pdfs
 pdfs=($(du -a ~/Desktop/Books/ | grep ".pdf" | cut -f2-))
@@ -10,10 +13,16 @@ choice=$(printf "%s\n" "${pdfs[@]##*/}" | \
 	dmenu -l 20 -nf "#4D6784" -nb "#08111C" \
 	-fn "xos4 Terminus:style=Bold:pixelsize=20" \
 	-p "" -sf "#00AAAD" -sb "#004953" -i)
-for i in "${!pdfs[@]}" ; do
-    if [[ $choice == "${pdfs[$i]##*/}" ]] ; then
-		notify-send "$choice" -i /home/danny/Pictures/1200px-PDF_file_icon.svg
-		zathura "${pdfs[$i]}" &
-		exit 0
-    fi
-done
+if [[ ! -z $choice ]]
+then
+	for i in "${!pdfs[@]}" ; do
+		if [[ $choice == "${pdfs[$i]##*/}" ]] ; then
+			_bookType="${pdfs[$i]%/*}"
+			notify-send "$(echo -e "${_bookType##*/}\n" \
+				| tr '[:lower:]' '[:upper:]')" "$choice" \
+				-i $(echo "${PWD}/pdf.png")
+			zathura "${pdfs[$i]}" &
+			break
+		fi
+	done
+fi
