4e4c1874d6dcf155e6dd3246d4fe902029c09ecd
Daniel Mendoza <dannymebd@gmail.com>
2022-05-11 17:30:01 -0500

    Validating readPdf.sh script
    - Modify readPdf.sh script
    - Creating images directory and adding screenshots
        * notification.png
        * dmenu_pdfs.png
    - Modify README.md file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# PDFs opened from dmenu selection 
+# Open PDF files using dmenu
+
+In order to run this script you need to have installed
+
+* zathura (document viewer) - **required**
+* dmenu (dynamic menu) - **required**
+* libnotify (notification daemon) - **optional**
+
+Please edit the script file before you run it.
+
+- The lines are specifically commented so you can change accordingly.
+
+## Read pdf selector preview
+![dmenu](images/dmenu_pdfs.png)
+
+## Notification
+<img src="images/notification.png" width="200">
--- readPdf.sh
+++ readPdf.sh
@@ -4,22 +4,25 @@
 # Robotics Engineer
 
 
-# Notify-send display the folder and file that you select!
+# Notify-send will display the folder and file that you select!
 
 IFS=$'\n'
 declare -a pdfs
-pdfs=($(du -a ~/Desktop/Books/ | grep ".pdf" | cut -f2-))
+pdfs=($(du -a ~/Downloads | grep ".pdf" | cut -f2-)) # Replace ~/Dowloads to any directory where you save or store your pdf files.
+
+# Replace font with one of your preference, try monospace in case any font is working.
 choice=$(printf "%s\n" "${pdfs[@]##*/}" | \
 	dmenu -l 20 -nf "#4D6784" -nb "#08111C" \
-	-fn "xos4 Terminus:style=Bold:pixelsize=20" \
-	-p "" -sf "#00AAAD" -sb "#004953" -i)
+    -fn "Iosevka:size=16" \
+    -p "BOOK: " -sf "#00AAAD" -sb "#004953" -i)
 if [[ ! -z $choice ]]
 then
 	for i in "${!pdfs[@]}" ; do
 		if [[ $choice == "${pdfs[$i]##*/}" ]] ; then
 			_bookType="${pdfs[$i]%/*}"
+            # comment next 3 lines if you do not have installed libnotify pkg.
 			notify-send "$(echo -e "${_bookType##*/}\n" \
-				| tr '[:lower:]' '[:upper:]')" "$choice" \
+                | tr '[:lower:]' '[:upper:]')" "$choice" \
 				-i $(echo "${PWD}/pdf.png")
 			zathura "${pdfs[$i]}" &
 			break
